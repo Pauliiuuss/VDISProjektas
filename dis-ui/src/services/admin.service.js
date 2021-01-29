7cd65c011b7242cf1b2670cc2c662e401a3c3e44
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-01-29 17:04:01 +0200

    navbar fix with username+role view and added fontawesome libraries

--- dis-ui/src/services/admin.service.js
+++ dis-ui/src/services/admin.service.js
@@ -1,12 +1,12 @@
-import axios from "axios";
-import authHeader from "./auth-header";
+import axios from 'axios';
+import authHeader from './auth-header';
 
-// const API_URL = process.env.PUBLIC_URL + "/api/admin/";
-const API_URL = "http://localhost:8080/api/admin/";
+// const API_URL = process.env.PUBLIC_URL + '/api/admin/';
+const API_URL = 'http://localhost:8080/api/admin/';
 
 class AdminService {
   createUser(body) {
-    return axios.post(API_URL + "create", body, { headers: authHeader() });
+    return axios.post(API_URL + 'create', body, { headers: authHeader() });
   }
 }
 
