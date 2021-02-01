07eee0eb1ece6a398d8eb2a466a802a0e89bc85b
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-02-01 17:51:19 +0200

    addeded data.sgl edited application.properties db name and other

---
+++ dis-app/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO ROLES VALUES (1, 'ROLE_ADMIN'),
+                         (2, 'ROLE_SPEC'),
+                         (3, 'ROLE_PARENT');
+INSERT INTO USERS VALUES (1, '$2a$10$Mul.9QjG.OObcNNrMZnsSuWFkygsYIcz8tZKJwqIjJErItg19Fy5S', 'admin');
+INSERT INTO USER_ROLES VALUES (1,1);
