28b4f2f4549777dbb78b31d93001fb0af7e2553b
Paulius <paulius.astrauskas97@gmail.com>
2021-02-03 16:42:48 +0200

    Changed password enryption library from BCrypt to SHA256

--- dis-app/src/main/java/it/akademija/config/WebSecurityConfig.java
+++ dis-app/src/main/java/it/akademija/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -49,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return new MessageDigestPasswordEncoder("SHA-256");
 	}
 
 	@Override
