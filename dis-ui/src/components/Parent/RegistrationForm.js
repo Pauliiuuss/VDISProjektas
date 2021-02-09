6f3dde0d1bd458d9b2f4e48535fbebab92d0093d
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-02-09 16:40:44 +0200

    form priorities options

--- dis-ui/src/components/Parent/RegistrationForm.js
+++ dis-ui/src/components/Parent/RegistrationForm.js
@@ -10,12 +10,96 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 export default class RegistrationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vardasAtstovas1: '',
+      pavardeAtstovas1: '',
+      kodasAtstovas1: '',
+      adresasAtstovas1: '',
+      miestasAtstovas1: '',
+      telAtstovas1: '',
+      elpastasAtstovas1: '',
+      vardasAtstovas2: '',
+      pavardeAtstovas2: '',
+      kodasAtstovas2: '',
+      adresasAtstovas2: '',
+      miestasAtstovas2: '',
+      telAtstovas2: '',
+      elpastasAtstovas2: '',
+      vaikoVardas: '',
+      vaikoPavarde: '',
+      vaikoKodas: '',
+      gimimoData: '',
+      vaikoAdresas: '',
+      vaikoMiestas: '',
+
+      //   selectKindergarten: {
+      //     prioritetas1: '',
+      //     prioritetas2: '',
+      //     prioritetas3: '',
+      //     prioritetas4: '',
+      //     prioritetas5: '',
+      //   },
+      //   selectPriorityOptions: {
+      //     deklaruota: '',
+      //     ivaikintas: '',
+      //     daugiavaike: '',
+      //     mokosiTevas: '',
+      //     nedarbingumas: '',
+      //   },
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.history.push('/dis-app/application');
+
+    this.setState({
+      vardasAtstovas1: '',
+      pavardeAtstovas1: '',
+      kodasAtstovas1: '',
+      adresasAtstovas1: '',
+      miestasAtstovas1: '',
+      telAtstovas1: '',
+      elpastasAtstovas1: '',
+      vardasAtstovas2: '',
+      pavardeAtstovas2: '',
+      kodasAtstovas2: '',
+      adresasAtstovas2: '',
+      miestasAtstovas2: '',
+      telAtstovas2: '',
+      elpastasAtstovas2: '',
+      vaikoVardas: '',
+      vaikoPavarde: '',
+      vaikoKodas: '',
+      gimimoData: '',
+      vaikoAdresas: '',
+      vaikoMiestas: '',
+      kindergarten: '',
+    });
+  };
+
+  kindergartenDropdownSelect = (e) => {
+    e.preventDefault();
+    this.setState({
+      kindergarten: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
         <Navbar />
         <h2 className=" text-center text-success my-5">Naujas prašymas</h2>
-        <form className="container text-secondary">
+        <form className="container text-secondary" onSubmit={this.handleSubmit}>
           <div className="row">
             <div className="col-lg-6">
               <h3 className="mb-4 text-center">Vaiko atstovas 1</h3>
@@ -35,6 +119,9 @@ export default class RegistrationForm extends Component {
                     name="vardasAtstovas1"
                     className="form-control"
                     placeholder="Vardas"
+                    value={this.state.vardasAtstovas1}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -54,6 +141,9 @@ export default class RegistrationForm extends Component {
                     name="pavardeAtstovas1"
                     className="form-control"
                     placeholder="Pavardė"
+                    value={this.state.pavardeAtstovas1}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -73,6 +163,9 @@ export default class RegistrationForm extends Component {
                     name="kodasAtstovas1"
                     className="form-control"
                     placeholder="Asmens kodas"
+                    value={this.state.kodasAtstovas1}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -92,6 +185,9 @@ export default class RegistrationForm extends Component {
                     name="adresasAtstovas1"
                     className="form-control"
                     placeholder="Adresas"
+                    value={this.state.adresasAtstovas1}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -111,6 +207,9 @@ export default class RegistrationForm extends Component {
                     name="miestasAtstovas1"
                     className="form-control"
                     placeholder="Miestas"
+                    value={this.state.miestasAtstovas1}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -130,6 +229,9 @@ export default class RegistrationForm extends Component {
                     name="telAtstovas1"
                     className="form-control"
                     placeholder="Telefonas"
+                    value={this.state.telAtstovas1}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -149,6 +251,9 @@ export default class RegistrationForm extends Component {
                     name="elpastasAtstovas1"
                     className="form-control"
                     placeholder="El.paštas"
+                    value={this.state.elpastasAtstovas1}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -171,6 +276,8 @@ export default class RegistrationForm extends Component {
                     name="vardasAtstovas2"
                     className="form-control"
                     placeholder="Vardas"
+                    value={this.state.vardasAtstovas2}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -190,6 +297,8 @@ export default class RegistrationForm extends Component {
                     name="pavardeAtstovas2"
                     className="form-control"
                     placeholder="Pavardė"
+                    value={this.state.pavardeAtstovas2}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -209,6 +318,8 @@ export default class RegistrationForm extends Component {
                     name="kodasAtstovas1"
                     className="form-control"
                     placeholder="Asmens kodas"
+                    value={this.state.kodasAtstovas2}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -228,6 +339,8 @@ export default class RegistrationForm extends Component {
                     name="adresasAtstovas2"
                     className="form-control"
                     placeholder="Adresas"
+                    value={this.state.adresasAtstovas2}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -247,6 +360,8 @@ export default class RegistrationForm extends Component {
                     name="miestasAtstovas2"
                     className="form-control"
                     placeholder="Miestas"
+                    value={this.state.miestasAtstovas2}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -266,6 +381,8 @@ export default class RegistrationForm extends Component {
                     name="telAtstovas2"
                     className="form-control"
                     placeholder="Telefonas"
+                    value={this.state.telAtstovas2}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -285,6 +402,8 @@ export default class RegistrationForm extends Component {
                     name="elpastasAtstovas2"
                     className="form-control"
                     placeholder="El.paštas"
+                    value={this.state.elpastasAtstovas2}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -309,6 +428,9 @@ export default class RegistrationForm extends Component {
                     name="vaikoVardas"
                     className="form-control"
                     placeholder="Vardas"
+                    value={this.state.vaikoVardas}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -328,6 +450,9 @@ export default class RegistrationForm extends Component {
                     name="vaikoPavarde"
                     className="form-control"
                     placeholder="Pavardė"
+                    value={this.state.vaikoPavarde}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -347,6 +472,9 @@ export default class RegistrationForm extends Component {
                     name="vaikoKodas"
                     className="form-control"
                     placeholder="Asmens kodas"
+                    value={this.state.vaikoKodas}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -366,6 +494,9 @@ export default class RegistrationForm extends Component {
                     name="gimimoData"
                     className="form-control"
                     placeholder="Gimimo data"
+                    value={this.state.gimimoData}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -385,6 +516,9 @@ export default class RegistrationForm extends Component {
                     name="vaikoAdresas"
                     className="form-control"
                     placeholder="Adresas"
+                    value={this.state.vaikoAdresas}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -404,6 +538,9 @@ export default class RegistrationForm extends Component {
                     name="vaikoMiestas"
                     className="form-control"
                     placeholder="Miestas"
+                    value={this.state.vaikoMiestas}
+                    onChange={this.handleChange}
+                    required="required"
                   />
                 </div>
               </div>
@@ -414,12 +551,17 @@ export default class RegistrationForm extends Component {
               <h3 className="mb-4 mt-5 text-center">
                 Pasirinkite darželį (prioriteto mažėjimo tvarka):
               </h3>
-              <div className="form-group ">
-                <div className="form-inline">
-                  <label className="col-2" htmlFor="1prioritetas">
+              <div className="form-group">
+                <div className="form-inline mb-4">
+                  <label className="col-2" htmlFor="prioritetas1">
                     1 prioritetas
                   </label>
-                  <select className="form-control col-5" id="1prioritetas">
+                  <select
+                    className="form-control col-5"
+                    id="selectprior1"
+                    value={this.state.kindergarten}
+                    onChange={this.kindergartenDropdownSelect}
+                  >
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -427,11 +569,11 @@ export default class RegistrationForm extends Component {
                     <option>5</option>
                   </select>
                 </div>
-                <div className="form-inline">
-                  <label className="col-2" htmlFor="1prioritetas">
-                    1 prioritetas
+                <div className="form-inline mb-4">
+                  <label className="col-2" htmlFor="prioritetas2">
+                    2 prioritetas
                   </label>
-                  <select className="form-control col-5" id="1prioritetas">
+                  <select className="form-control col-5" id="prioritetas2">
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -439,11 +581,11 @@ export default class RegistrationForm extends Component {
                     <option>5</option>
                   </select>
                 </div>
-                <div className="form-inline">
-                  <label className="col-2" htmlFor="1prioritetas">
-                    1 prioritetas
+                <div className="form-inline mb-4">
+                  <label className="col-2" htmlFor="prioritetas3">
+                    3 prioritetas
                   </label>
-                  <select className="form-control col-5" id="1prioritetas">
+                  <select className="form-control col-5" id="prioritetas3">
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -451,11 +593,11 @@ export default class RegistrationForm extends Component {
                     <option>5</option>
                   </select>
                 </div>
-                <div className="form-inline">
-                  <label className="col-2" htmlFor="1prioritetas">
-                    1 prioritetas
+                <div className="form-inline mb-4">
+                  <label className="col-2" htmlFor="prioritetas4">
+                    4 prioritetas
                   </label>
-                  <select className="form-control col-5" id="1prioritetas">
+                  <select className="form-control col-5" id="prioritetas4">
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -463,11 +605,11 @@ export default class RegistrationForm extends Component {
                     <option>5</option>
                   </select>
                 </div>
-                <div className="form-inline">
-                  <label className="col-2" htmlFor="1prioritetas">
-                    1 prioritetas
+                <div className="form-inline mb-4">
+                  <label className="col-2" htmlFor="prioritetas5">
+                    5 prioritetas
                   </label>
-                  <select className="form-control col-5" id="1prioritetas">
+                  <select className="form-control col-5" id="prioritetas5">
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -478,6 +620,78 @@ export default class RegistrationForm extends Component {
               </div>
             </div>
           </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <h3 className="mb-4 mt-5 text-center">
+                Pažymėkite visas atitinkančias sąlygas:
+              </h3>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="deklaruota"
+                  name="deklaruota"
+                />
+                <label className="form-check-label" htmlFor="defaultCheck1">
+                  Deklaruota gyvenomoji vieta Vilniaus m.
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="ivaikintas"
+                  name="ivaikintas"
+                />
+                <label className="form-check-label" htmlFor="defaultCheck1">
+                  Vaikas įvaikintas
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="daugiavaike"
+                  name="daugiavaike"
+                />
+                <label className="form-check-label" htmlFor="defaultCheck1">
+                  Šeimoje yra 3 ir daugiau vaikų, besimokančių bendro ugdymo
+                  programose
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="mokosTevas"
+                  name="mokosiTevas"
+                />
+                <label className="form-check-label" htmlFor="defaultCheck1">
+                  Vienas iš tėvų(globėjų) mokosi bendro ugdymo mokykloje
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  name="nedarbingumas"
+                  id="nedarbingumas"
+                />
+                <label className="form-check-label" htmlFor="defaultCheck1">
+                  Vienas iš tėvų(globėjų) turi ne daugiau kaip 40 proc.
+                  nedarbingumo lygio
+                </label>
+              </div>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-success my-5">
+            Pateikti prašymą
+          </button>
         </form>
       </div>
     );
