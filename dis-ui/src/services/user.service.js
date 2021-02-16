ca38419bbcf6e3e1610375ac683b5df5b6a36c14
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-02-16 11:53:21 +0200

    url fixes

--- dis-ui/src/services/user.service.js
+++ dis-ui/src/services/user.service.js
@@ -1,20 +1,20 @@
-import axios from "axios";
-import authHeader from "./auth-header";
+import axios from 'axios';
+import authHeader from './auth-header';
 
 // const API_URL = process.env.PUBLIC_URL + '/api/userdata/';
 const API_URL = 'http://localhost:8080/api/userdata/';
 
 class UserService {
   getUserData(id) {
-    return axios.get(API_URL + "get/" + id, { headers: authHeader() });
+    return axios.get(API_URL + 'get/' + id, { headers: authHeader() });
   }
 
   addData(id, body) {
-    return axios.post(API_URL + "add/" + id, body, { headers: authHeader() });
+    return axios.post(API_URL + 'add/' + id, body, { headers: authHeader() });
   }
 
   updatePassword(id, oldPassword, newPasword) {
-    return axios.put(API_URL + "password/" + id, [oldPassword, newPasword], {
+    return axios.put(API_URL + 'password/' + id, [oldPassword, newPasword], {
       headers: authHeader(),
     });
   }
