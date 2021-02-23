8c5c32d4a82e9766600eab82d4842d8fc1a0b0a9
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-02-23 20:49:50 +0200

    File upload date to db, alerts from back and front check

--- dis-ui/src/services/upload-files.service.js
+++ dis-ui/src/services/upload-files.service.js
@@ -1,25 +1,25 @@
-import axios from "axios";
-import authHeader from "./auth-header";
-import { LINK } from "./LINK";
+import axios from 'axios';
+import authHeader from './auth-header';
+import { LINK } from './LINK';
 
-const API_URL = LINK + "/api/documents/";
+const API_URL = LINK + '/api/documents/';
 
 class UploadFilesService {
   upload(id, file, onUploadProgress) {
     let formData = new FormData();
 
-    formData.append("file", file);
+    formData.append('file', file);
 
-    return axios.post(API_URL + "add/" + id, formData, {
+    return axios.post(API_URL + 'add/' + id, formData, {
       headers: {
-        "Content-Type": "multipart/form-data",
+        'Content-Type': 'multipart/form-data',
       },
       onUploadProgress,
     });
   }
 
   getFiles() {
-    return axios.get(API_URL + "all", { headers: authHeader() });
+    return axios.get(API_URL + 'all', { headers: authHeader() });
   }
 }
 export default new UploadFilesService();
