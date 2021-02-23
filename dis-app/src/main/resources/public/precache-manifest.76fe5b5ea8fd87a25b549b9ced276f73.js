89604d71c306991c09f6bb82ff9b0cdf9ccc1e87
Erlandas <erlandaslisauskas@gmail.com>
2021-02-23 19:56:15 +0200

    added PasswordReset component in Front-end

---
+++ dis-app/src/main/resources/public/precache-manifest.76fe5b5ea8fd87a25b549b9ced276f73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a3f2e1e57108b61fcf9c080e479a41",
+    "url": "/dis-app/index.html"
+  },
+  {
+    "revision": "3757ffc244c111b0bcec",
+    "url": "/dis-app/static/css/2.2358bba0.chunk.css"
+  },
+  {
+    "revision": "31c7ba1ff04147967fe8",
+    "url": "/dis-app/static/css/main.1dc783b7.chunk.css"
+  },
+  {
+    "revision": "3757ffc244c111b0bcec",
+    "url": "/dis-app/static/js/2.75b38de2.chunk.js"
+  },
+  {
+    "revision": "dd0be973842ce9c7c5a895b6cfc5578e",
+    "url": "/dis-app/static/js/2.75b38de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c7ba1ff04147967fe8",
+    "url": "/dis-app/static/js/main.5196ff41.chunk.js"
+  },
+  {
+    "revision": "633388aa2defb1fbac1e",
+    "url": "/dis-app/static/js/runtime-main.1f242d6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dis-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dis-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dis-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dis-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dis-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "86652faf9551526e46e1a8ed7e88088e",
+    "url": "/dis-app/static/media/logo.86652faf.png"
+  }
+]);
