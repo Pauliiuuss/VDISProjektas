ddc2303b2b40fa57595620d45620e0dcd3dea616
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-02-25 10:19:04 +0200

    fix of front All.js paths and db number restored back to VDISDB00009

--- dis-ui/src/App.js
+++ dis-ui/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import Login from "./components/login.component";
-import MainAfterLogin from "./components/MainAferLogin";
-import UnderConstruction from "./components/underConstruction";
-import RegistrationForm from "./components/Parent/RegistrationForm";
-import UserUpdateForm from "./components/UserDetailUpdate/UserDetailUpdate";
-import PasswordReset from "./components/PasswordReset";
+import Login from './components/login.component';
+import MainAfterLogin from './components/MainAferLogin';
+import UnderConstruction from './components/underConstruction';
+import RegistrationForm from './components/Parent/ChildRegistration/RegistrationForm';
+import UserUpdateForm from './components/UserDetailUpdate/UserDetailUpdate';
+import PasswordReset from './components/PasswordReset';
 import Documents from './components/Spec/ListOfDocuments/Documents';
 
 class App extends Component {
   render() {
-    document.title = "DarželiųInformacinėSistema";
+    document.title = 'DarželiųInformacinėSistema';
     return (
       <div>
         <Switch>
           <Route
             exact
-            path={["/dis-app/", "/dis-app/login"]}
+            path={['/dis-app/', '/dis-app/login']}
             component={Login}
           />
           <Route path="/dis-app/forgot" component={PasswordReset} />
