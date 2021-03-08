5a436d87f69d90a6412c7ca27db5dc5db441c68a
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-03-08 21:10:31 +0200

    removed under construction file from statistic, added navigation bar

--- dis-ui/src/App.js
+++ dis-ui/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import ChildFormsQueue from "./components/Spec/ChildFormsQueue";
-import Login from "./components/login.component";
-import MainAfterLogin from "./components/MainAferLogin";
-import UnderConstruction from "./components/underConstruction";
-import RegistrationForm from "./components/Parent/ChildRegistration/RegistrationForm";
-import UserUpdateForm from "./components/UserDetailUpdate/UserDetailUpdate";
-import Documents from "./components/Spec/ListOfDocuments/Documents";
+import ChildFormsQueue from './components/Spec/ChildFormsQueue';
+import Login from './components/login.component';
+import MainAfterLogin from './components/MainAferLogin';
+import RegistrationForm from './components/Parent/ChildRegistration/RegistrationForm';
+import UserUpdateForm from './components/UserDetailUpdate/UserDetailUpdate';
+import Documents from './components/Spec/ListOfDocuments/Documents';
+import Statistic from './components/statistic/Statistic';
 
 class App extends Component {
   render() {
-    document.title = "DarželiųInformacinėSistema";
+    document.title = 'DarželiųInformacinėSistema';
     return (
       <div>
         <Switch>
           <Route
             exact
-            path={["/dis-app/", "/dis-app/login"]}
+            path={['/dis-app/', '/dis-app/login']}
             component={Login}
           />
           <Route exact path="/dis-app/home" component={MainAfterLogin} />
@@ -27,7 +27,7 @@ class App extends Component {
           <Route path="/dis-app/docs" component={Documents} />
           <Route path="/dis-app/mydata" component={UserUpdateForm} />
           <Route path="/dis-app/queue" component={ChildFormsQueue} />
-          <Route path="/dis-app/statistic" component={UnderConstruction} />
+          <Route path="/dis-app/statistic" component={Statistic} />
         </Switch>
       </div>
     );
---
+++ dis-ui/src/components/statistic/Statistic.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/navbar.component';
+import Chart from './Chart';
+
+const Statistic = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Chart />
+    </React.Fragment>
+  );
+};
+
+export default Statistic;
