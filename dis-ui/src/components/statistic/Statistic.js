6eb98952bf1e03c7a86e681b68268aaa0dda6d09
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-03-10 09:11:11 +0200

    Authorization added to statistic

--- dis-ui/src/components/statistic/Statistic.js
+++ dis-ui/src/components/statistic/Statistic.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AuthService from '../../services/auth.service';
 import Navbar from '../../components/navbar.component';
 import Chart from './Chart';
 
-const Statistic = () => {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <Chart />
-    </React.Fragment>
-  );
-};
+export default class Statistic extends Component {
+  state = {
+    redirect: null,
+    userReady: false,
+    currentUser: { username: '' },
+    roles: [],
+  };
 
-export default Statistic;
+  async componentDidMount() {
+    const currentUser = await AuthService.getCurrentUser();
+
+    if (!currentUser) {
+      this.props.history.push('/dis-app/');
+      window.location.reload();
+    } else
+      this.setState({
+        currentUser: currentUser,
+        userReady: true,
+        roles: currentUser.roles,
+      });
+  }
+
+  render() {
+    if (this.state.roles.includes('ROLE_SPEC'))
+      return (
+        <React.Fragment>
+          <Navbar />
+          <Chart />
+        </React.Fragment>
+      );
+    if (this.state.roles.includes('ROLE_PARENT'))
+      return (
+        <React.Fragment>
+          <Navbar />
+          <Chart currentUser={this.state.currentUser} />
+        </React.Fragment>
+      );
+    return '';
+  }
+}
